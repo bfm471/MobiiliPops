86a412ad2c1bbd8a03ef798a3d0d1493c57f62af
Satu Kulta <satu.kulta@myy.haaga-helia.fi>
2024-10-28 17:56:34 +0200

    Kansiorakennetta + LoginScreen pohja

---
+++ screens/LoginScreen.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function LoginScreen() {
+
+
+    return (
+        <View>
+            <Text>Tämä on LoginScreen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
