4eb1c66cbe6c8bd88884431d8c5d698d5421e29b
Satu Kulta <satu.kulta@myy.haaga-helia.fi>
2024-11-08 18:35:03 +0200

    Lisätty Firebase + conffaus

---
+++ configs/FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfZXvstA6ZYDkGZ6pbuxLTLiTEioml6T8",
+  authDomain: "pops-eda8d.firebaseapp.com",
+  projectId: "pops-eda8d",
+  storageBucket: "pops-eda8d.firebasestorage.app",
+  messagingSenderId: "706318821947",
+  appId: "1:706318821947:web:168e8555031eacb6f78a1e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
