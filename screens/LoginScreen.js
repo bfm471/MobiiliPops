e43ae1b94eb672f9cbc20a69dc9014fd5c345b72
Satu Kulta <satu.kulta@myy.haaga-helia.fi>
2024-11-08 20:08:50 +0200

    Toteutettu sisäänkirjautuminen + siirto Home-näkymään

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,19 +1,40 @@
 import { Alert, StyleSheet, Text, View } from "react-native";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRef, useState, useEffect } from "react";
+
+import { app } from "../configs/FirebaseConfig";
 import Header from "../components/Header";
 import TextInputCustom from "../components/TextInput";
 import ButtonCustom from "../components/Button";
-import { useRef, useState } from "react";
 import LoginSigninLink from "../components/LoginSigninLink";
 
-export default function LoginScreen({ navigation }) {
+export default function LoginScreen({ navigation, route }) {
+    const auth = getAuth(app);
+    const prevUsername = route?.params?.username;
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const passwordRef = useRef(null);
 
     const handleLogin = () => {
-        Alert.alert("Nappi painettu!", username + " " + password);
+        signInWithEmailAndPassword(auth, username, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            setUsername('');
+            setPassword('');
+            console.log(user.email);
+            navigation.navigate('Home');
+        }).catch((error) => {
+            console.log("ERROR with signin:", error);
+            Alert.alert("Error occured while signing in.")
+        })
     }
+
+    useEffect(() => {
+        if(prevUsername != null)  {
+            setUsername(prevUsername);
+        }
+    });
 
     return (
         <View style={styles.container}>
@@ -25,16 +46,16 @@ export default function LoginScreen({ navigation }) {
                     onChangeText={(text) => setUsername(text)}
                     value={username}
                     textContentType='username'
-                    returnKeyType='next'
-                    onSubmitEditing={() => passwordRef.current.focus()}
-                    label='Username'
+                    returnKeyType='done'
+                    // onSubmitEditing={() => passwordRef.current.focus()} // vai sulkee vain näppäimistön?
+                    label='Email / Username'
                 />
                 <TextInputCustom 
                     onChangeText={(text) => setPassword(text)}
                     value={password}
                     textContentType='password'
                     returnKeyType='done'
-                    onSubmitEditing={() => handleLogin()} // vai sulkee vain näppäimistön?
+                    // onSubmitEditing={() => handleLogin()} // vai sulkee vain näppäimistön?
                     secureTextEntry={true}
                     ref={passwordRef}
                     label='Password'
@@ -42,7 +63,7 @@ export default function LoginScreen({ navigation }) {
                 <ButtonCustom
                     title="Log In"
                     onPress={handleLogin}
-                    disabled={username.length < 3 || password.length < 5}
+                    disabled={username.length < 3 || password.length < 6}
                 />
             </View>
             <View>
