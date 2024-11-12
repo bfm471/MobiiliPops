bb9770892dbb8ff5cc3644d7570e9ed65a3416d5
Satu Kulta <satu.kulta@myy.haaga-helia.fi>
2024-11-12 09:06:15 +0200

    Tykättyjen vitsien hakutoiminto

--- services/DatabaseService.js
+++ services/DatabaseService.js
@@ -1,11 +1,11 @@
-import { getDatabase, push, ref, remove, set } from "firebase/database";
+import { getDatabase, onValue, ref, remove, set } from "firebase/database";
 
 import { app } from '../configs/FirebaseConfig';
 import { getUserCreds } from "./UserService";
 
 const database = getDatabase(app);
 
-export const saveItem = async (item) => {
+export const saveFavoriteJoke = async (item) => {
     console.log("TALLENNA VITSI")
 
     const creds = await getUserCreds();
@@ -25,14 +25,33 @@ export const saveItem = async (item) => {
 }
 
 export const deleteItem = async (item) => {
-    console.log("POISTA VITSI");
+    console.log("POISTA VITSI", item);
 
     const creds = await getUserCreds();
     const userId = creds.id;
 
     try {
-        remove(ref(database, `${userId}/${item.id}`))
+        remove(ref(database, `${userId}/${item.id ?? item.key}`))
     } catch (error) {
         console.log("ERROR with delete item", error);
     }
+}
+
+export const readItems = async (setJokes) => {
+    console.log("TUO VITSIT");
+
+    const creds = await getUserCreds();
+    const userId = creds.id;
+    const userRef = ref(database, `${userId}`);
+
+    onValue(userRef, (snapshot) => {
+        if (snapshot.exists()) {
+            const data = snapshot.val();
+            const keys = Object.keys(data);
+            const keysAndData = Object.values(data).map((obj, index) => {
+                return { ...obj, key: keys[index] }
+            })
+            setJokes(keysAndData);
+        }
+    })
 }
